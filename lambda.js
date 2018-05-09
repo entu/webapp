211d6cdc79a53a1e0381a80432dd5d3ce70c327e
Argo Roots <argo.roots@lhv.ee>
2018-05-09 18:26:03 +0300

    Set base path and fic mime type on S3 copy

--- lambda.js
+++ lambda.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const aws = require('aws-sdk')
+const mime = require('mime')
 const s3 = new aws.S3()
 
 exports.handler = async (event) => {
@@ -8,13 +9,19 @@ exports.handler = async (event) => {
 
   const bucket = event.Records[0].s3.bucket.name
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
+  const type = mime.getType(key)
 
   await s3.copyObject({
     ACL: 'public-read',
     Bucket: bucket,
     CopySource: `${bucket}/${key}`,
-    Key: key.replace('build/', 'public/')
+    Key: key.replace('build/', 'public/'),
+    ContentType: type,
+    MetadataDirective: 'REPLACE',
+    Metadata: {
+      'Content-Type': type
+    }
   }).promise()
 
-  console.log(key)
+  console.log('Key: ' + key + ' Type: ' + type)
 }
