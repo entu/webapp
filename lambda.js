1c3786803c025f752a4eaedd59dea21331b57ed3
Argo Roots <argo.roots@lhv.ee>
2018-05-09 12:15:40 +0300

    Lambda to asynk/await

--- lambda.js
+++ lambda.js
@@ -1,30 +1,18 @@
 'use strict'
 
-console.log('Loading function')
-
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
 
+exports.handler = async (event) => {
+  // console.log('Received event:', JSON.stringify(event, null, 2))
 
+  const bucket = event.Records[0].s3.bucket.name
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
 
-exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2))
-
-    const bucket = event.Records[0].s3.bucket.name
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
-    const params = {
-        ACL: 'public-read',
-        Bucket: bucket,
-        CopySource: `${bucket}/${key}`,
-        Key: key.replace('build/', 'public/')
-    }
-
-    s3.copyObject(params, (err, data) => {
-        if (err) {
-            console.log(err)
-            callback(err)
-        } else {
-            callback(null, key)
-        }
-    })
+  await s3.copyObject({
+    ACL: 'public-read',
+    Bucket: bucket,
+    CopySource: `${bucket}/${key}`,
+    Key: key.replace('build/', 'public/')
+  }).prototype()
 }
