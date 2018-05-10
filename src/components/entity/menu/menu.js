06b5e7d0f794b38c77003fe003073b608e2df326
Argo Roots <argo.roots@lhv.ee>
2018-05-10 14:32:15 +0300

    Rewrite menu

--- src/components/entity/menu/menu.js
+++ src/components/entity/menu/menu.js
@@ -1,174 +1,114 @@
 import { get } from 'axios'
 
-var locale
-
-const getValue = (valueList) => {
-  let values = []
-
-  valueList.forEach((v) => {
-    if (!v.language || v.language === locale) {
-      values.push(v.string)
-    }
-  })
-
-  return values[0]
-}
-
-const getUser = (route, callback) => {
-  const account = route.params.account
-  const accounts = JSON.parse(sessionStorage.getItem('accounts'))
-
-  if (!accounts || !accounts[account] || !accounts[account]._id || !accounts[account].token) {
-    return callback(null, null)
-  }
-
-  const options = {
-    headers: {
-      Authorization: `Bearer ${accounts[account].token}`
-    },
-    params: {
-      props: [
-        'forename.string',
-        'forename.language',
-        'surname.string',
-        'surname.language',
-        'photo._id'
-      ].join(',')
-    }
-  }
-
-  get(`https://api.entu.app/entity/${accounts[account]._id}`, options)
-    .then(response => {
-      callback(null, {
-        name: [getValue(response.data.forename), getValue(response.data.forename)].join(' '),
-        photo: response.data.photo[0]._id
-      })
-    })
-    .catch(err => {
-      callback(err)
-    })
-}
-
-const getPhoto = (_id, route, callback) => {
-  const account = route.params.account
-  const accounts = JSON.parse(sessionStorage.getItem('accounts'))
-
-  if (!accounts || !accounts[account] || !accounts[account]._id || !accounts[account].token) {
-    return callback(null, null)
-  }
-
-  const options = {
-    headers: {
-      Authorization: `Bearer ${accounts[account].token}`
-    }
-  }
-
-  get(`https://api.entu.app/property/${_id}`, options)
-    .then(response => {
-      callback(null, response.data.url)
-    })
-    .catch(err => {
-      callback(err)
-    })
-}
-
-const getMenu = (route, callback) => {
-  const account = route.params.account
-  const accounts = JSON.parse(sessionStorage.getItem('accounts'))
-  let options = {
-    params: {
-      account: account,
-      '_type.string': 'menu',
-      props: [
-        'group.string',
-        'group.language',
-        'title.string',
-        'title.language',
-        'query.string'
-      ].join(',')
-    }
-  }
-
-  if (accounts && accounts[account] && accounts[account].token) {
-    options.headers = {
-      Authorization: `Bearer ${accounts[account].token}`
-    }
-  }
-
-  get(`https://api.entu.app/entity`, options)
-    .then(response => {
-      if (!response.data || !response.data.entities) { return callback(new TypeError('No menu')) }
-
-      let menu = {}
-      let menuQueries = {}
-
-      response.data.entities.forEach((entity) => {
-        let group = getValue(entity.group)
-        if (!menu[group]) {
-          menu[group] = {
-            title: getValue(entity.group),
-            links: [],
-            active: false
-          }
-        }
-        menu[group].links.push({
-          _id: entity._id,
-          title: getValue(entity.title),
-          query: entity._id
-        })
-        menuQueries[entity._id] = getValue(entity.query)
-      })
-
-      callback(null, {
-        menu: Object.values(menu),
-        queries: menuQueries
-      })
-    }).catch(err => {
-      callback(err)
-    })
-}
-
 export default {
   name: 'EntityMenu',
   created () {
-    locale = this.locale
-
-    getUser(this.$route, (err, person) => {
-      if (err) { return console.log(err) }
-
-      if (person && person.photo) {
-        getPhoto(person.photo, this.$route, (err, url) => {
-          if (err) {
-            console.error(err)
-          } else {
-            this.user.photo = url
-          }
-          this.user = person
-        })
-      }
-    })
-
-    getMenu(this.$route, (err, menu) => {
-      if (err) { return console.log(err) }
-
-      this.menu = menu.menu
-      this.menuQueries = menu.queries
-      this.menu[0].active = true
-    })
+    this.getUser()
+    this.getMenu()
   },
   data () {
     return {
-      token: null,
-      closed: false,
       user: {
         name: null,
         photo: null
       },
+      closed: false,
       menu: [],
+      menuQueries: {},
       entity: this.$route.params.entity
     }
   },
-  computed: {
+  methods: {
+    getUser () {
+      if (!this.userId || !this.token) { return }
 
+      const headers = {
+        Authorization: `Bearer ${this.token}`
+      }
+      const options = {
+        headers: headers,
+        params: {
+          props: [
+            'forename.string',
+            'forename.language',
+            'surname.string',
+            'surname.language',
+            'photo._id'
+          ].join(',')
+        }
+      }
+
+      get(`https://api.entu.app/entity/${this.userId}`, options)
+        .then(response => {
+          this.user.name = [this.getValue(response.data.forename), this.getValue(response.data.forename)].join(' ')
+
+          if (!response.data.photo) { return }
+
+          get(`https://api.entu.app/property/${response.data.photo[0]._id}`, { headers: headers })
+            .then(response => {
+              this.user.photo = response.data.url
+              delete this.user.photoId
+            })
+            .catch(err => {
+              console.error(err)
+            })
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    },
+    getMenu () {
+      let options = {
+        params: {
+          account: this.account,
+          '_type.string': 'menu',
+          props: [
+            'group.string',
+            'group.language',
+            'title.string',
+            'title.language',
+            'query.string'
+          ].join(',')
+        }
+      }
+
+      if (this.token) {
+        options.headers = {
+          Authorization: `Bearer ${this.token}`
+        }
+      }
+
+      get(`https://api.entu.app/entity`, options)
+        .then(response => {
+          if (!response.data || !response.data.entities) { return }
+
+          let menu = {}
+
+          response.data.entities.forEach((entity) => {
+            let group = this.getValue(entity.group)
+
+            if (!menu[group]) {
+              menu[group] = {
+                title: this.getValue(entity.group),
+                links: [],
+                active: false
+              }
+            }
+
+            menu[group].links.push({
+              _id: entity._id,
+              title: this.getValue(entity.title),
+              query: entity._id
+            })
+
+            this.menuQueries[entity._id] = this.getValue(entity.query)
+          })
+
+          this.menu = Object.values(menu)
+          this.menu[0].active = true
+        }).catch(err => {
+          console.error(err)
+        })
+    }
   }
 }
--- src/mixins.js
+++ src/mixins.js
@@ -1,25 +1,44 @@
 export default {
-    computed: {
-        authenticated () {
-            const account = this.$route.params.account
-            const accounts = JSON.parse(sessionStorage.getItem('accounts'))
-            return accounts && accounts[account] && accounts[account].token
-        },
-        locales () {
-            return ['et', 'en']
-        },
-        selectableLocales () {
-            return this.locales.filter(l => l !== this.locale)
-        },
-        locale () {
-            return this.$i18n.locale
-        }
-    },
-    methods: {
-        setLocale (val) {
-            localStorage.setItem('locale', val)
-            this.$i18n.locale = val
-            this.$router.go()
-        }
+  data () {
+    return {
+      locales: ['et', 'en']
     }
+  },
+  computed: {
+    account () {
+      return this.$route.params.account
+    },
+    token () {
+      const accounts = JSON.parse(sessionStorage.getItem('accounts'))
+      return accounts && accounts[this.account] && accounts[this.account].token
+    },
+    userId () {
+      const accounts = JSON.parse(sessionStorage.getItem('accounts'))
+      return accounts && accounts[this.account] && accounts[this.account]._id
+    },
+    selectableLocales () {
+      return this.locales.filter(l => l !== this.locale)
+    },
+    locale () {
+      return this.$i18n.locale
+    }
+  },
+  methods: {
+    setLocale (val) {
+      localStorage.setItem('locale', val)
+      this.$i18n.locale = val
+      this.$router.go()
+    },
+    getValue (valueList) {
+      let values = []
+
+      valueList.forEach((v) => {
+        if (!v.language || v.language === this.locale) {
+          values.push(v.string)
+        }
+      })
+
+      return values[0]
+    }
+  }
 }
