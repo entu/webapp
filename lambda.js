721515709e284db28c1ca3c9d91f559bbffa8419
Argo Roots <argo.roots@lhv.ee>
2018-05-10 07:26:56 +0300

    Set mime type and cache on S3 copy

--- lambda.js
+++ lambda.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const aws = require('aws-sdk')
-const mime = require('mime')
 const s3 = new aws.S3()
 
-exports.handler = async (event) => {
-  // console.log('Received event:', JSON.stringify(event, null, 2))
+const mimeTypes = {
+  'css': 'text/css',
+  'html': 'text/html',
+  'ico': 'image/x-icon',
+  'js': 'application/javascript',
+  'png': 'image/png',
+  'txt': 'text/plain'
+}
 
+exports.handler = async (event) => {
   const bucket = event.Records[0].s3.bucket.name
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
-  const type = mime.getType(key)
+  const type = mimeTypes[key.split('.').pop()]
+  const cache = key.endsWith('index.html') ? 'no-cache' : 'max-age=604800, must-revalidate'
 
   await s3.copyObject({
     ACL: 'public-read',
@@ -17,10 +24,8 @@ exports.handler = async (event) => {
     CopySource: `${bucket}/${key}`,
     Key: key.replace('build/', 'public/'),
     ContentType: type,
-    MetadataDirective: 'REPLACE',
-    Metadata: {
-      'Content-Type': type
-    }
+    CacheControl: cache,
+    MetadataDirective: 'REPLACE'
   }).promise()
 
   console.log('Key: ' + key + ' Type: ' + type)
