16a4096b35aa11484cf7a8efd6f2d305800e58f1
Argo Roots <argo.roots@lhv.ee>
2018-05-11 09:27:30 +0300

    Sort untitled menu groups to top

--- src/components/entity/menu/menu.js
+++ src/components/entity/menu/menu.js
@@ -56,8 +56,8 @@ export default {
     },
     getMenu () {
       const sorter = (a, b) => {
-        if (a.title < b.title) { return -1 }
-        if (a.title > b.title) { return 1 }
+        if (!a.title || a.title < b.title) { return -1 }
+        if (!b.title || a.title > b.title) { return 1 }
         return 0
       }
 
