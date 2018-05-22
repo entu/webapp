ceb924cd55e4e0d2a93dc9c41a2611ea8ddc3ac9
Argo Roots <argo.roots@lhv.ee>
2018-05-22 14:50:02 +0300

    Remove Db size from file size

--- src/components/account/info/info.js
+++ src/components/account/info/info.js
@@ -4,68 +4,52 @@ export default {
   name: 'accountInfo',
   data () {
     return {
-      entities: 0,
-      deletedEntities: 0,
-      totalEntities: 0,
-
-      entitiesStr: '',
-      deletedEntitiesStr: '',
-
-      entitiesStyle: '',
-
-      properties: 0,
-      deletedProperties: 0,
-      totalproperties: 0,
-
-      propertiesStr: '',
-      deletedPropertiesStr: '',
-
-      propertiesStyle: '',
-
       dbSize: 0,
-      filesSize: 0,
-      deletedFilesSize: 0,
-      totalFilesSize: 0,
-
       dbSizeStr: '',
-      filesSizeStr: '',
-      deletedFilesSizeStr: '',
-
       dbSizeStyle: '',
+      deletedEntities: 0,
+      deletedEntitiesStr: '',
+      deletedFilesSize: 0,
+      deletedFilesSizeStr: '',
+      deletedProperties: 0,
+      deletedPropertiesStr: '',
+      entities: 0,
+      entitiesStr: '',
+      entitiesStyle: '',
+      filesSize: 0,
+      filesSizeStr: '',
       filesSizeStyle: '',
+      properties: 0,
+      propertiesStr: '',
+      propertiesStyle: ''
     }
   },
   created () {
     this.axios.get('/account').then((response) => {
       this.entities = _get(response, 'data.entities', 0)
       this.deletedEntities = _get(response, 'data.deletedEntities', 0)
-      this.totalEntities = this.entities + this.deletedEntities
 
       this.entitiesStr = this.entities.toLocaleString(this.locale)
       this.deletedEntitiesStr = this.deletedEntities.toLocaleString(this.locale)
-
-      this.entitiesStyle = `width: ${Math.round(this.entities * 100 / this.totalEntities)}%`
+      this.entitiesStyle = `width: ${Math.round(this.entities * 100 / (this.entities + this.deletedEntities))}%`
 
       this.properties = _get(response, 'data.properties', 0)
       this.deletedProperties = _get(response, 'data.deletedProperties', 0)
-      this.totalProperties = this.properties + this.deletedProperties
 
       this.propertiesStr = this.properties.toLocaleString(this.locale)
       this.deletedPropertiesStr = this.deletedProperties.toLocaleString(this.locale)
+      this.propertiesStyle = `width: ${Math.round(this.properties * 100 / (this.properties + this.deletedProperties))}%`
 
-      this.propertiesStyle = `width: ${Math.round(this.properties * 100 / this.totalProperties)}%`
-
-      this.dbSize = _get(response, 'data.dbSize', 0)
       this.filesSize = _get(response, 'data.filesSize', 0)
       this.deletedFilesSize = _get(response, 'data.deletedFilesSize', 0)
-      this.totalFilesSize = this.dbSize + this.filesSize + this.deletedFilesSize
 
-      this.dbSizeStr = this.getReadableFileSize(this.dbSize)
       this.filesSizeStr = this.getReadableFileSize(this.filesSize)
       this.deletedFilesSizeStr = this.getReadableFileSize(this.deletedFilesSize)
+      this.filesSizeStyle = `width: ${Math.round(this.filesSize * 100 / (this.filesSize + this.deletedFilesSize))}%`
 
-      this.dbSizeStyle = `width: ${Math.round(this.dbSize * 100 / this.totalFilesSize)}%`
-      this.filesSizeStyle = `width: ${Math.round(this.filesSize * 100 / this.totalFilesSize)}%`
+      this.dbSize = _get(response, 'data.dbSize', 0)
+      this.dbSizeStr = this.getReadableFileSize(this.dbSize)
+      this.dbSizeStyle = `width: ${Math.round(this.dbSize * 100 / (this.dbSize + this.filesSize + this.deletedFilesSize))}%`
     })
   },
   computed: {
