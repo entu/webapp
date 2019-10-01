6db0cb7af92cd803ec93d5d5b17987babca68f68
Argo Roots <argo@roots.ee>
2019-10-01 08:55:25 +0300

    Fix logout

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -119,7 +119,7 @@ export default {
     logOut () {
       this.setAccounts()
       this.authenticating = false
-      this.$router.push({ name: 'auth' })
+      window.location.reload()
     }
   }
 }
