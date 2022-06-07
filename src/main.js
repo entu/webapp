a50fcb48b28ae898041784aede254a30b6e23cab
Argo Roots <argo.roots@adm.ee>
2022-06-07 09:01:33 +0300

    Log Git SHA

--- src/main.js
+++ src/main.js
@@ -8,3 +8,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+
+console.log(`%cX-Entu-Version:%c ${import.meta.env.VITE_APP_GIT_SHA}`, 'font-weight:bold;color:green;font-family:monospace', 'color:green;font-family:monospace')
