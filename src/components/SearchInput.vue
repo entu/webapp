a2d9acec9389e01fb5a9ef7bc100979a7d0adf5c
Argo Roots <argo@roots.ee>
2022-06-08 14:15:28 +0300

    Add search input

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed, defineEmits, defineProps } from 'vue'
+import { Search as SearchIcon } from '@vicons/carbon'
+
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  }
+})
+
+const searchText = computed({
+  get () {
+    return props.modelValue
+  },
+  set (val) {
+    emit('update:modelValue', val)
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center">
+    <label
+      for="search"
+      class="p-3"
+    >
+      <search-icon class="h-5 w-5 text-slate-400" />
+    </label>
+    <input
+      id="search"
+      v-model="searchText"
+      placeholder="Search Entity"
+      class="w-full py-3 bg-transparent placeholder:italic placeholder:text-slate-400 focus:outline-none"
+    >
+  </div>
+</template>
--- src/views/AccountLayout.vue
+++ src/views/AccountLayout.vue
@@ -1,16 +1,21 @@
 <script setup>
-import { watch, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { NLayout, NLayoutContent } from 'naive-ui'
+import { watch, onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { NLayout } from 'naive-ui'
 
 import { useStore } from '@/store'
 import LeftMenu from '@/components/LeftMenu.vue'
+import SearchInput from '@/components/SearchInput.vue'
 
 const store = useStore()
 const route = useRoute()
+const router = useRouter()
+
+const search = ref('')
 
 onMounted(async () => {
   store.account = route.params.account
+  search.value = route.query.q
   store.getMenu()
 })
 
@@ -18,11 +23,25 @@ watch(() => route.params.account, (value) => {
   store.account = route.params.account
   store.getMenu()
 })
+
+watch(() => route.query.q, (value) => {
+  search.value = value
+})
+
+watch(() => search.value, (value) => {
+  if (!value) {
+    const newQuery = { ...route.query }
+    delete newQuery.q
+    router.replace({ query: newQuery })
+  } else {
+    router.replace({ query: { ...route.query, q: value } })
+  }
+})
 </script>
 
 <template>
   <n-layout
-    position="absolute"
+    class="h-full w-full"
     has-sider
   >
     <left-menu
@@ -30,13 +49,18 @@ watch(() => route.params.account, (value) => {
       :accounts="store.accounts"
       :is-authenticated="store.isAuthenticated"
     />
-    <n-layout>
-      <n-layout-content
-        position="absolute"
-        content-style="height:100%"
+    <div class="w-full flex flex-col">
+      <div class="h-12 flex items-center justify-between border-b border-slate-300">
+        <search-input
+          v-model="search"
+          class="w-64"
+        />
+      </div>
+      <div
+        class="grow  overflow-y-auto"
       >
         <router-view />
-      </n-layout-content>
-    </n-layout>
+      </div>
+    </div>
   </n-layout>
 </template>
