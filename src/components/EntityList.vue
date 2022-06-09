970424ce587660e0fe5bbb71765291a832d14782
Argo Roots <argo@roots.ee>
2022-06-09 17:38:14 +0300

    Show thumbnail in list

--- src/components/EntityList.vue
+++ src/components/EntityList.vue
@@ -18,9 +18,15 @@ defineProps({
     <router-link
       v-for="entity in entities"
       :key="entity._id"
-      class="h-12 px-2 flex items-center border-b border-slate-100"
+      class="h-12 px-2 flex gap-2 items-center border-b border-slate-100"
       :to="{ name: 'entity', params: { ...route.params, entity: entity._id }, query: route.query }"
     >
+      <img
+        v-if="entity._thumbnail"
+        :src="entity._thumbnail"
+        class="w-8 h-8 rounded-full"
+        alt=""
+      >
       <div class="truncate whitespace-nowrap overflow-hidden ">
         {{ getValue(entity.name) }}
       </div>
