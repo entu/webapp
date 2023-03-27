d86256ef73b6476590b9b5db3898fcfe0c5db8b9
Argo Roots <argo@roots.ee>
2023-03-27 13:36:38 +0300

    Show "Add new object" menu

--- components/entity/tools-menu.vue
+++ components/entity/tools-menu.vue
@@ -3,41 +3,95 @@ import { NButton, NPopover } from 'naive-ui'
 
 const props = defineProps({
   account: { type: String, required: true },
-  entityId: { type: String, required: true },
+  entityId: { type: String, default: null },
   right: { type: String, default: null },
-  typeId: { type: String, required: true }
+  typeId: { type: String, default: null }
 })
 
 const { t } = useI18n()
+const route = useRoute()
 
 const addChilds = ref([])
+const addDefaults = ref([])
 
-const addOptions = computed(() => addChilds.value.map(x => ({
-  value: x._id,
-  label: getValue(x.label) || getValue(x.name)
-})).sort((a, b) => a.label.localeCompare(b.label)))
+const addChildOptions = computed(() => {
+  const result = []
+
+  if (['owner', 'editor', 'expander'].includes(props.right) && addChilds.value.length > 0) {
+    result.push({
+      title: t('addUnderThis'),
+      options: addChilds.value.map(x => ({
+        value: x._id,
+        label: getValue(x.label) || getValue(x.name)
+      })).sort((a, b) => a.label.localeCompare(b.label))
+    })
+  }
+
+  if (addDefaults.value.length > 0) {
+    result.push({
+      title: t('addNew'),
+      options: addDefaults.value.map(x => ({
+        value: x._id,
+        label: getValue(x.label) || getValue(x.name)
+      })).sort((a, b) => a.label.localeCompare(b.label))
+    })
+  }
+
+  return result
+})
+
+async function loadAddDefaults () {
+  const { entities: menuEntities } = await apiGetEntities({
+    '_type.string': 'menu',
+    'query.string': location.search.substring(1),
+    props: '_id'
+  })
+
+  if (menuEntities.length === 0) return
+
+  const { entities } = await apiGetEntities({
+    '_type.string': 'entity',
+    'add_from.reference': menuEntities[0]._id,
+    props: 'name,label'
+  })
+
+  addDefaults.value = entities
+}
+
+watch(() => route.query, loadAddDefaults, { deep: true })
 
 onMounted(async () => {
-  const { entities } = await apiGetEntities({
-    'add_from.reference': props.entityId,
-    props: 'name,label'
-  })
+  if (props.entityId) {
+    const { entities } = await apiGetEntities({
+      'add_from.reference': props.entityId,
+      props: 'name,label'
+    })
 
-  const { entities: types } = await apiGetEntities({
-    'add_from.reference': props.typeId,
-    props: 'name,label'
-  })
+    addChilds.value = [...addChilds.value, ...entities.filter(x => !addChilds.value.some(y => y._id === x._id))]
+  }
 
-  addChilds.value = [...entities, ...types.filter(x => !entities.some(y => y._id === x._id))]
+  if (props.typeId) {
+    const { entities } = await apiGetEntities({
+      'add_from.reference': props.typeId,
+      props: 'name,label'
+    })
+
+    addChilds.value = [...addChilds.value, ...entities.filter(x => !addChilds.value.some(y => y._id === x._id))]
+  }
+
+  loadAddDefaults()
 })
 </script>
 
 <template>
-  <div class="h-12 mx-2 flex items-center justify-end border-b border-gray-300">
+  <div
+    v-if="entityId || addChildOptions.length > 0"
+    class="h-12 mx-2 flex items-center justify-end border-b border-gray-300"
+  >
     <n-popover
-      v-if="['owner', 'editor', 'expander'].includes(right) && addOptions.length > 0"
-      content-style="padding:0"
-      header-style="padding:0"
+      v-if="addChildOptions.length > 0"
+      content-style="padding:0;border:none"
+      footer-style="padding:0;border:none"
     >
       <template #trigger>
         <n-button quaternary>
@@ -47,25 +101,28 @@ onMounted(async () => {
         </n-button>
       </template>
 
-      <template #header>
-        <div class="py-2 px-4 font-bold">
-          {{ t('addUnderThis') }}
-        </div>
-      </template>
-
       <div
-        class="w-full flex flex-col"
+        v-for="addGroup in addChildOptions"
+        :key="addGroup.title"
+        class="w-full flex flex-col border-b last:border-b-0"
         vertical
       >
+        <div class="py-2 px-4 font-bold text-[#1E434C]">
+          {{ addGroup.title }}
+        </div>
         <nuxt-link
-          v-for="child in addOptions"
+          v-for="child in addGroup.options"
           :key="child.value"
           class="py-2 px-4 hover:bg-gray-50 cursor-pointer"
-          :to="{ path: `/${account}/${entityId}/add/${child.value}` }"
+          :to="{ path: `/${account}/${entityId || '_'}/add/${child.value}` }"
         >
           {{ child.label }}
         </nuxt-link>
       </div>
+
+      <template #footer>
+        <div />
+      </template>
     </n-popover>
 
     <n-popover v-if="['owner', 'editor'].includes(right)">
@@ -80,7 +137,7 @@ onMounted(async () => {
       {{ t('edit') }}
     </n-popover>
 
-    <n-popover>
+    <n-popover v-if="entityId">
       <template #trigger>
         <n-button quaternary>
           <template #icon>
@@ -92,7 +149,7 @@ onMounted(async () => {
       {{ t('duplicate') }}
     </n-popover>
 
-    <n-popover>
+    <n-popover v-if="entityId">
       <template #trigger>
         <n-button quaternary>
           <template #icon>
@@ -104,7 +161,7 @@ onMounted(async () => {
       {{ t('parents') }}
     </n-popover>
 
-    <n-popover>
+    <n-popover v-if="entityId">
       <template #trigger>
         <n-button quaternary>
           <template #icon>
@@ -120,12 +177,14 @@ onMounted(async () => {
 
 <i18n lang="yaml">
   en:
-    addUnderThis: Add children
+    addNew: Add new object
+    addUnderThis: Add child object
     edit: Edit
     duplicate: Duplicate
     parents: Parents
     rights: User rights
   et:
+    addNew: Lisa uus objekt
     addUnderThis: Lisa alamobjekt
     edit: Muuda
     duplicate: Dubleeri
--- pages/[account]/index.vue
+++ pages/[account]/index.vue
@@ -20,50 +20,60 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    v-if="!isQuery && stats && authenticated"
-    class="h-full w-full lg:w-1/2 xl:w-1/2 md:min-w-fit px-8 md:mx-auto flex flex-col justify-center"
-    vertical
-  >
-    <stats-bar
-      class="my-3"
-      color="rgb(23,162,184)"
-      rail-color="rgba(23,162,184,.3)"
-      :a-label="t('entities')"
-      :b-label="t('deleted')"
-      :a-value="stats.entities"
-      :b-value="stats.deletedEntities"
-    />
-    <stats-bar
-      class="my-3"
-      color="rgb(255,193,7)"
-      rail-color="rgba(255,193,7,.3)"
-      :a-label="t('properties')"
-      :b-label="t('deleted')"
-      :a-value="stats.properties"
-      :b-value="stats.deletedProperties"
-    />
-    <stats-bar
-      class="my-3"
-      is-bytes
-      color="rgb(40,167,69)"
-      rail-color="rgba(40,167,69,.3)"
-      :a-label="t('files')"
-      :b-label="t('deleted')"
-      :a-value="stats.filesSize"
-      :b-value="stats.deletedFilesSize"
-    />
-    <stats-bar
-      class="my-3"
-      color="rgb(108,117,125)"
-      rail-color="rgba(108,117,125,.3)"
-      show-total
-      :a-label="t('requests')"
-      :b-label="t('limit')"
-      :a-value="stats.dbSize"
-      :b-value="Math.ceil(stats.dbSize / Math.pow(10, stats.dbSize.toString().length - 1)) * Math.pow(10, stats.dbSize.toString().length - 1) - stats.dbSize"
-    />
-  </div>
+  <transition>
+    <div class="h-full flex flex-col">
+      <div
+        v-if="isQuery && authenticated"
+        class="h-12"
+      >
+        <entity-tools-menu :account="account" />
+      </div>
+      <div
+        v-if="!isQuery && stats && authenticated"
+        class="h-full w-full lg:w-1/2 xl:w-1/2 md:min-w-fit px-8 md:mx-auto flex flex-col justify-center"
+        vertical
+      >
+        <stats-bar
+          class="my-3"
+          color="rgb(23,162,184)"
+          rail-color="rgba(23,162,184,.3)"
+          :a-label="t('entities')"
+          :b-label="t('deleted')"
+          :a-value="stats.entities"
+          :b-value="stats.deletedEntities"
+        />
+        <stats-bar
+          class="my-3"
+          color="rgb(255,193,7)"
+          rail-color="rgba(255,193,7,.3)"
+          :a-label="t('properties')"
+          :b-label="t('deleted')"
+          :a-value="stats.properties"
+          :b-value="stats.deletedProperties"
+        />
+        <stats-bar
+          class="my-3"
+          is-bytes
+          color="rgb(40,167,69)"
+          rail-color="rgba(40,167,69,.3)"
+          :a-label="t('files')"
+          :b-label="t('deleted')"
+          :a-value="stats.filesSize"
+          :b-value="stats.deletedFilesSize"
+        />
+        <stats-bar
+          class="my-3"
+          color="rgb(108,117,125)"
+          rail-color="rgba(108,117,125,.3)"
+          show-total
+          :a-label="t('requests')"
+          :b-label="t('limit')"
+          :a-value="stats.dbSize"
+          :b-value="Math.ceil(stats.dbSize / Math.pow(10, stats.dbSize.toString().length - 1)) * Math.pow(10, stats.dbSize.toString().length - 1) - stats.dbSize"
+        />
+      </div>
+    </div>
+  </transition>
 </template>
 
 <i18n lang="yaml">
