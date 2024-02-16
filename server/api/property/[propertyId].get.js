aea8d9de1efd41ad09d2b31f08953d6b3546390f
Argo Roots <argo@roots.ee>
2024-02-16 15:24:00 +0200

    Add /api/property

---
+++ server/api/property/[propertyId].get.js
@@ -0,0 +1,63 @@
+export default defineEventHandler(async (event) => {
+  const entu = event.context.entu
+
+  const property = await entu.db.collection('property').findOne({
+    _id: entu._id,
+    deleted: { $exists: false }
+  })
+
+  if (!property) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Property not found'
+    })
+  }
+
+  const entity = await entu.db.collection('entity').findOne({
+    _id: property.entity
+  }, {
+    projection: {
+      _id: false,
+      access: true
+    }
+  })
+
+  if (!entity) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Entity not found'
+    })
+  }
+
+  const access = (entity.access || []).map(s => s.toString())
+
+  if (property.public) {
+    if (!access.includes('public')) {
+      throw createError({
+        statusCode: 403,
+        statusMessage: 'Not a public property'
+      })
+    }
+  } else if (!access.includes(entu.user)) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'User not in any rights property'
+    })
+  }
+
+  if (property.s3) {
+    property.url = await getSignedDownloadUrl(property.s3)
+
+    delete property.s3
+  }
+
+  if (property.type === 'entu_api_key') {
+    property.string = '***'
+  }
+
+  if (property.url && event.queryStringParameters?.download) {
+    await sendRedirect(event, property.url, 302)
+  } else {
+    return property
+  }
+})
