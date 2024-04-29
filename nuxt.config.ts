3af768d31d109b41fe5742e02f024f0a4b838263
Argo Roots <argo@roots.ee>
2024-04-29 09:04:13 +0300

    Revert "Add LiveChat"
    This reverts commit bbdfa9739b8b4c4c9b6e9bed8ab3170d6366e9f6.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,9 +10,6 @@ export default defineNuxtConfig({
       title: process.env.NUXT_PUBLIC_TITLE || 'Entu',
       link: [
         { rel: 'icon', type: 'image/png', href: '/logo.png' }
-      ],
-      script: [
-        { children: 'window.__lc=window.__lc||{},window.__lc.license=17768025,function(n,t,c){function e(n){return l._h?l._h.apply(null,n):l._q.push(n)}var l={_q:[],_h:null,_v:"2.0",on:function(){e(["on",c.call(arguments)])},once:function(){e(["once",c.call(arguments)])},off:function(){e(["off",c.call(arguments)])},get:function(){if(!l._h)throw Error("[LiveChatWidget] You can\'t use getters before load.");return e(["get",c.call(arguments)])},call:function(){e(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};n.__lc.asyncInit||l.init(),n.LiveChatWidget=n.LiveChatWidget||l}(window,document,[].slice);' }
       ]
     }
   },
