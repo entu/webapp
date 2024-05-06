3b4dcdccf7046cab1496b878dd003d1d10f7204f
Argo Roots <argo@roots.ee>
2024-05-06 16:19:42 +0300

    Revert "Remove cookie path"
    This reverts commit d8ca5d5a2256f994d6d75bcbc62cfeaf7fa72878.

--- server/api/auth/index.get.js
+++ server/api/auth/index.get.js
@@ -108,6 +108,7 @@ export default defineEventHandler(async (event) => {
   setCookie(event, 'token', token, {
     httpOnly: true,
     maxAge: 60 * 60 * 48,
+    path: '/api',
     sameSite: 'None',
     secure: true
   })
