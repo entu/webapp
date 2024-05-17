3d235a35d98dbb505876695f9dd3371a3bc45699
Argo Roots <argo@roots.ee>
2024-05-17 13:49:42 +0300

    Add /billing page

---
+++ pages/billing.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { t } = useI18n()
+
+definePageMeta({ layout: 'blank' })
+
+onMounted(async () => {
+  useHead({ title: t('title') })
+
+  await navigateTo('https://billing.stripe.com/p/login/00g02XgGnc7F4lGeUU', { external: true, open: { target: '_blank' } })
+})
+</script>
+
+<template>
+  <div />
+</template>
+
+<i18n lang="yaml">
+  en:
+    title: Billing
+  et:
+    title: Arveldus
+</i18n>
