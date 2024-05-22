76eb889e66d360fbf2f65bd59a37f9c0968aa4a3
Argo Roots <argo@roots.ee>
2024-05-22 15:23:35 +0300

    Set billing_customer property in Stripe webhook

--- server/api/stripe.post.js
+++ server/api/stripe.post.js
@@ -1,5 +1,37 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  console.log(JSON.stringify(body))
+  if (body.type === 'checkout.session.completed') {
+    const { customer, reference } = body.data?.object
+
+    if (!customer) {
+      console.error('No customer found in checkout')
+
+      return
+    }
+
+    if (!reference) {
+      console.error('No reference found in checkout')
+
+      return
+    }
+
+    const [db, user] = reference.split('-')
+
+    const entu = {
+      account: db,
+      db: await connectDb(db),
+      systemUser: true
+    }
+
+    const { _id: databaseId } = await entu.db.collection('entity').findOne({
+      'private._type.string': 'database'
+    }, { projection: { _id: true } })
+
+    await setEntity(entu, databaseId, [
+      { type: 'billing_customer', string: customer }
+    ])
+  }
+
+  console.error('Unhandled Stripe event:', body.type)
 })
