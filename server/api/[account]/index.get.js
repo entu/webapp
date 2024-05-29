c83648600f03d11443665ceda4ec32a0a047127f
Argo Roots <argo@roots.ee>
2024-05-29 09:02:51 +0300

    Fix deleted files stats and make queries in parallel

--- server/api/[account]/index.get.js
+++ server/api/[account]/index.get.js
@@ -1,52 +1,49 @@
 export default defineEventHandler(async (event) => {
   const entu = event.context.entu
-
-  const stats = await entu.db.stats()
-
-  const entities = await entu.db.collection('entity').countDocuments()
-
-  const deletedEntities = await entu.db.collection('property').aggregate([{
-    $match: {
-      type: '_deleted'
-    }
-  }, {
-    $group: {
-      _id: '$entity'
-    }
-  }, {
-    $count: 'count'
-  }]).toArray()
-
-  const properties = await entu.db.collection('property').aggregate([{
-    $group: {
-      _id: { $gt: ['$deleted', null] },
-      count: { $sum: 1 }
-    }
-  }]).toArray()
-
-  const files = await entu.db.collection('property').aggregate([{
-    $match: {
-      filesize: { $exists: true }
-    }
-  }, {
-    $group: {
-      _id: { $gt: ['$deleted', null] },
-      count: { $sum: 1 },
-      filesize: { $sum: '$filesize' }
-    }
-  }]).toArray()
-
-  const database = await entu.db.collection('entity').findOne({ 'private._type.string': 'database' }, {
-    projection: {
-      'private.name.string': true,
-      'private.billing_entities_limit.number': true,
-      'private.billing_data_limit.number': true,
-      'private.billing_requests_limit.number': true
-    }
-  })
-
   const date = new Date().toISOString()
-  const requests = await entu.db.collection('stats').findOne({ date: date.substring(0, 7), function: 'ALL' })
+
+  const [
+    stats,
+    database,
+    entities,
+    deletedEntities,
+    properties,
+    deletedProperties,
+    files,
+    deletedFiles,
+    requests
+  ] = await Promise.all([
+    entu.db.stats(),
+    entu.db.collection('entity').findOne({ 'private._type.string': 'database' }, {
+      projection: {
+        'private.name.string': true,
+        'private.billing_entities_limit.number': true,
+        'private.billing_data_limit.number': true,
+        'private.billing_requests_limit.number': true
+      }
+    }),
+    entu.db.collection('entity').countDocuments(),
+    entu.db.collection('property').aggregate([
+      { $match: { type: '_deleted' } },
+      { $group: { _id: '$entity' } },
+      { $count: 'count' }
+    ]).toArray(),
+    entu.db.collection('property').countDocuments({ deleted: { $exists: false } }),
+    entu.db.collection('property').countDocuments({ deleted: { $exists: true } }),
+    entu.db.collection('property').aggregate([
+      { $match: { deleted: { $exists: false }, filesize: { $exists: true } } },
+      { $lookup: { from: 'entity', localField: 'entity', foreignField: '_id', as: 'entities' } },
+      { $match: { entities: { $not: { $size: 0 } } } },
+      { $group: { _id: null, count: { $sum: 1 }, filesize: { $sum: '$filesize' } } }
+    ]).toArray(),
+    entu.db.collection('property').aggregate([
+      { $match: { filesize: { $exists: true } } },
+      { $lookup: { from: 'entity', localField: 'entity', foreignField: '_id', as: 'entities' } },
+      { $match: { $or: [{ entities: { $size: 0 } }, { deleted: { $exists: true } }] } },
+      { $group: { _id: null, count: { $sum: 1 }, filesize: { $sum: '$filesize' } } }
+    ]).toArray(),
+    entu.db.collection('stats').findOne({ date: date.substring(0, 7), function: 'ALL' })
+  ])
 
   return {
     entities: {
@@ -55,8 +52,8 @@ export default defineEventHandler(async (event) => {
       limit: database?.private?.billing_entities_limit?.at(0)?.number || 0
     },
     properties: {
-      usage: properties.find(e => e._id === false)?.count || 0,
-      deleted: properties.find(e => e._id === true)?.count || 0
+      usage: deletedProperties || 0,
+      deleted: properties || 0
     },
     requests: {
       usage: requests?.count || 0,
@@ -64,8 +61,8 @@ export default defineEventHandler(async (event) => {
       limit: Math.ceil(requests?.count / Math.pow(10, requests?.count.toString().length - 1)) * Math.pow(10, requests?.count.toString().length - 1) || 0
     },
     files: {
-      usage: files.find(e => e._id === false)?.filesize || 0,
-      deleted: files.find(e => e._id === true)?.filesize || 0,
+      usage: files?.at(0)?.filesize || 0,
+      deleted: deletedFiles?.at(0)?.filesize || 0,
       limit: (database?.private?.billing_data_limit?.at(0)?.number || 0) * 1e9
     },
     dbSize: stats.dataSize + stats.indexSize
