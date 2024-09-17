d59951e6919f9edcf986001d8ed90030c14e5f81
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-09-17 12:08:36 +0300

    #78 domain rights for property

--- server/api/[account]/entity/[entityId]/index.get.js
+++ server/api/[account]/entity/[entityId]/index.get.js
@@ -15,6 +15,7 @@ export default defineEventHandler(async (event) => {
       } else {
         fields[`private.${f}`] = true
         fields[`public.${f}`] = true
+        fields[`domain.${f}`] = true
       }
     })
     fields.access = true
