f3967d92d5174e54ff1f97107166fe31d7d97b49
Argo Roots <argo@roots.ee>
2025-04-25 19:30:59 +0300

    Do not log empty path

--- server/plugins/stats.js
+++ server/plugins/stats.js
@@ -2,6 +2,7 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('afterResponse', async (event) => {
     const date = new Date().toISOString()
     const entu = event.context.entu
+    const path = event.path.split('?').at(0).replace('/api', '').replace(`/${entu.account}`, '')
     const query = getQuery(event)
     const queryStr = new URLSearchParams(query).toString()
 
@@ -10,7 +11,7 @@ export default defineNitroPlugin((nitroApp) => {
       db: entu?.account,
       user: !entu?.systemUser && entu?.userStr ? entu?.userStr : undefined,
       method: event.method,
-      path: event.path.split('?').at(0).replace('/api', '').replace(`/${entu.account}`, ''),
+      path: path ? path : undefined,
       query: queryStr ? queryStr : undefined
     })
 
