dca0a873093cc8efdb260f9c3ac97d0a10910544
Argo Roots <argo@roots.ee>
2025-12-09 12:27:35 +0200

    Remove OpenSearch integration and update dependencies

--- server/plugins/stats.js
+++ server/plugins/stats.js
@@ -2,21 +2,22 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('afterResponse', async (event) => {
     const date = new Date().toISOString()
     const entu = event.context.entu
-    const path = event.path.split('?').at(0).replace('/api', '').replace(`/${entu.account}`, '')
-    const query = getQuery(event)
-    const queryStr = new URLSearchParams(query).toString()
-    const ip = getRequestIP(event, { xForwardedFor: true })
 
-    await indexOpenSearchDb('entu-requests', {
-      '@timestamp': date,
-      db: entu?.account,
-      user: !entu?.systemUser && entu?.userStr ? entu?.userStr : undefined,
-      method: event.method,
-      path: path ? path : '/',
-      query: queryStr ? decodeURIComponent(queryStr) : undefined,
-      ip: ip ? ip.replace(/^::ffff:/, '') : undefined,
-      browser: getHeader(event, 'user-agent')
-    })
+    // const path = event.path.split('?').at(0).replace('/api', '').replace(`/${entu.account}`, '')
+    // const query = getQuery(event)
+    // const queryStr = new URLSearchParams(query).toString()
+    // const ip = getRequestIP(event, { xForwardedFor: true })
+
+    // await indexOpenSearchDb('entu-requests', {
+    //   '@timestamp': date,
+    //   db: entu?.account,
+    //   user: !entu?.systemUser && entu?.userStr ? entu?.userStr : undefined,
+    //   method: event.method,
+    //   path: path ? path : '/',
+    //   query: queryStr ? decodeURIComponent(queryStr) : undefined,
+    //   ip: ip ? ip.replace(/^::ffff:/, '') : undefined,
+    //   browser: getHeader(event, 'user-agent')
+    // })
 
     if (!entu?.db) return
 
