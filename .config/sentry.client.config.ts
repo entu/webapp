6b01009a1a5a5b42c160b0e0616345eb62592f3b
Argo Roots <argo@roots.ee>
2026-08-17 14:06:42 +0300

    Disable Sentry in local development

--- .config/sentry.client.config.ts
+++ .config/sentry.client.config.ts
@@ -4,6 +4,8 @@ const { sentry, commitHash } = useRuntimeConfig().public
 
 Sentry.init({
   dsn: sentry.dsn,
+  // Never report errors from local development
+  enabled: !['localhost', '127.0.0.1'].includes(window.location.hostname),
   release: commitHash || undefined,
   tracesSampleRate: 0.1,
   tracePropagationTargets: ['api.entu.app']
